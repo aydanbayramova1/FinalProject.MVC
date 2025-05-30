2b68cff01589cbe28f5ed4a1a022a5fbf4b7ceaa
unknown <aydansb@code.edu.az>
2025-05-30 04:08:08 -0800

    continue

---
+++ FinalProjectMvc/FinalProjectMvc/Controllers/BlogDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectMvc.Controllers
+{
+    public class BlogDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
